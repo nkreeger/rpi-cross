4068ed6e5b197d0c1de9dca5807309037ff8b233
root <root@google.com>
2018-11-07 14:34:09 -0800

    Fix path

--- pi3_meson.sh
+++ pi3_meson.sh
@@ -1,5 +1,5 @@
 # Setup meson to cross-compile for RPI Arm7
 meson build/ \
-  --cross-file=$HOME/rpi-cross/cross \
+  --cross-file=$HOME/workspace/rpi-cross/cross \
   --prefix=/usr \
   --libdir=lib/arm-linux-gnueabihf \
